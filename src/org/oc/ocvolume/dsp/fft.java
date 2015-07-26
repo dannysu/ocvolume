27a2b9c9dcd160b78bab8f9630375a30e93f4bf9
Danny Su <contact@dannysu.com>
2015-07-26 12:36:15 -0700

    Import 1.0.1 from SourceForge

--- src/org/oc/ocvolume/dsp/fft.java
+++ src/org/oc/ocvolume/dsp/fft.java
@@ -1,26 +1,48 @@
 /*
 OC Volume - Java Speech Recognition Engine
-Copyright (C) 2002 OrangeCow organization
+Copyright (c) 2002-2004, OrangeCow organization
+All rights reserved.
 
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
+Redistribution and use in source and binary forms,
+with or without modification, are permitted provided
+that the following conditions are met:
 
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-Lesser General Public License for more details.
+* Redistributions of source code must retain the
+  above copyright notice, this list of conditions
+  and the following disclaimer.
+* Redistributions in binary form must reproduce the
+  above copyright notice, this list of conditions
+  and the following disclaimer in the documentation
+  and/or other materials provided with the
+  distribution.
+* Neither the name of the OrangeCow organization
+  nor the names of its contributors may be used to
+  endorse or promote products derived from this
+  software without specific prior written
+  permission.
 
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS
+AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
 
 Contact information:
 Please visit http://ocvolume.sourceforge.net.
 */
 
-package ocvolume.dsp;
+package org.oc.ocvolume.dsp;
 
 /**
  * last updated on June 15, 2002<br>
@@ -77,14 +99,17 @@ public class fft{
      * called by: fft
      */
     private static void FFT(){
+        if (numPoints == 1) return;
+
         final double pi = Math.PI;
         final int numStages = (int)(Math.log(numPoints) / Math.log(2));
         
-        int j = numPoints / 2;
+        int halfNumPoints = numPoints >> 1;
+        int j = halfNumPoints;
         
         // FFT time domain decomposition carried out by "bit reversal sorting" algorithm
         int k = 0;
-        for (int i = 0; i < numPoints - 2; i++){
+        for (int i = 1; i < numPoints - 2; i++){
             if (i < j){
                 // swap
                 double tempReal = real[j];
@@ -95,11 +120,11 @@ public class fft{
                 imag[i] = tempImag;
             }
             
-            k = numPoints / 2;
+            k = halfNumPoints;
             
             while ( k <= j ){
                 j -= k;
-                k /= 2;
+                k >>=1;
             }
             
             j += k;
@@ -107,8 +132,12 @@ public class fft{
 
         // loop for each stage
         for (int stage = 1; stage <= numStages; stage++){
-            int LE = (int)Math.pow(2, stage);
-            int LE2 = LE / 2;
+
+            int LE = 1;
+            for (int i = 0; i < stage; i++)
+                LE <<= 1;
+
+            int LE2 = LE >> 1;
             double UR = 1;
             double UI = 0;
             
